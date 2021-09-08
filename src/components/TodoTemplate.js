8db7cfb3111f33c74fc7034e65e1bde901ec1269
Gabul99 <hclee99@kaist.ac.kr>
2021-09-08 11:36:20 +0900

    날짜 양옆 버튼 추가

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,5 +1,6 @@
 import React from "react";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
+import { MdArrowBack, MdArrowForward } from "react-icons/all";
 
 const TodoTemplateBlock = styled.div`
   width: 512px;
@@ -17,11 +18,57 @@ const TodoTemplateBlock = styled.div`
   flex-direction: column;
 `
 
+const TodoTemplateButton = styled.button`
+  background: #38d9a9;
+
+  &:hover {
+    background: #63e6be;
+  }
+
+  &:active {
+    background: #20c997;
+  }
+
+  z-index: 5;
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+  display: block;
+  align-items: center;
+  justify-content: center;
+  font-size: 60px;
+  position: absolute;
+  left: 0px;
+  top: 10%;
+  transform: translate(-50%, 50%);
+  color: white;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${props =>
+          props.isForward &&
+          css`
+            left: 100%;
+          `}
+`
+
 function TodoTemplate({children}) {
   return (
-      <TodoTemplateBlock>
-        {children}
-      </TodoTemplateBlock>
+      <div>
+        <TodoTemplateBlock>
+          <TodoTemplateButton>
+            <MdArrowBack/>
+          </TodoTemplateButton>
+          {children}
+          <TodoTemplateButton isForward={true}>
+            <MdArrowForward/>
+          </TodoTemplateButton>
+        </TodoTemplateBlock>
+      </div>
   )
 }
 
